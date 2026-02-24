37961a0b7a7b520159a064909b9877b2097f7e06
Siddharth Shankar <siddharthshankar@Siddharths-MacBook-Air.local>
2026-02-23 23:04:58 -0700

    Fix Terraform backend region configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "s3" {
+    region = "us-east-2"
     # These values will be set by deployment scripts
     # For local development, they can be passed via -backend-config
   }
